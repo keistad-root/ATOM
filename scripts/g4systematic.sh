4aef0adcd2b8649f95f14869cb2aeb208d728365
keistad-root <asdff46@pusan.ac.kr>
2025-01-20 08:00:01 +0000

    auto commit at 2025-01-20 08:00:01

--- scripts/g4systematic.sh
+++ scripts/g4systematic.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
-./G4Simulation 41
-./G4Simulation 42
-./G4Simulation 43
-./G4Simulation 44
-./G4Simulation 45
-./G4Simulation 46
-./G4Simulation 47
-./G4Simulation 48
-./G4Simulation 49
-./G4Simulation 50
-./G4Simulation 51
+# ./G4Simulation 41
+# ./G4Simulation 42
+# ./G4Simulation 43
+# ./G4Simulation 44
+# ./G4Simulation 45
+# ./G4Simulation 46
+# ./G4Simulation 47
+# ./G4Simulation 48
+# ./G4Simulation 49
+# ./G4Simulation 50
+# ./G4Simulation 51
+
+./G4SimulationExtract L1F2AX_W082
+./G4SimulationExtract L1F2AX_W085
+./G4SimulationExtract L1F2AX_W088
+./G4SimulationExtract L1F2AX_W091
+./G4SimulationExtract L1F2AX_W094
+./G4SimulationExtract L1F2AX_W097
+./G4SimulationExtract L1F2AX_W100
+./G4SimulationExtract L1F2AX_W103
+./G4SimulationExtract L1F2AX_W106
+./G4SimulationExtract L1F2AX_W109
+./G4SimulationExtract L1F2AX_W112
