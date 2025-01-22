651359d61292bd181b256bb7b9b5fec621e32716
keistad-root <asdff46@pusan.ac.kr>
2025-01-22 15:00:01 +0900

    auto commit at 2025-01-22 15:00:01

--- scripts/g4analysis.sh
+++ scripts/g4analysis.sh
@@ -2,59 +2,26 @@
 
 cd ~/ATOM/build
 
-# ./G4SimulationAnalysis 1mm_2phi_noscreen
-# ./G4SimulationAnalysis 1mm_3phi_noscreen
-# ./G4SimulationAnalysis 1mm_4phi_noscreen
-# ./G4SimulationAnalysis 1mm_7phi_noscreen
-# ./G4SimulationAnalysis 1mm_nocollimator_noscreen
-# ./G4SimulationAnalysis 1mm_2phi_screen
-# ./G4SimulationAnalysis 1mm_3phi_screen
-# ./G4SimulationAnalysis 1mm_4phi_screen
-# ./G4SimulationAnalysis 1mm_7phi_screen
-# ./G4SimulationAnalysis 1mm_nocollimator_screen
+./G4SimulationAnalysis L1F2AX
+./G4SimulationAnalysis L1F2AO
+./G4SimulationAnalysis L1F3AX
+./G4SimulationAnalysis L1F3AO
+./G4SimulationAnalysis L1F4AX
+./G4SimulationAnalysis L1F4AO
+./G4SimulationAnalysis L1F7AX
+./G4SimulationAnalysis L1F7AO
+./G4SimulationAnalysis L1REFAX
+./G4SimulationAnalysis L1REFAO
 
-# ./G4SimulationAnalysis 3mm_2phi_noscreen
-# ./G4SimulationAnalysis 3mm_3phi_noscreen
-# ./G4SimulationAnalysis 3mm_4phi_noscreen
-# ./G4SimulationAnalysis 3mm_7phi_noscreen
-# ./G4SimulationAnalysis 3mm_nocollimator_noscreen
-# ./G4SimulationAnalysis 3mm_2phi_screen
-# ./G4SimulationAnalysis 3mm_3phi_screen
-# ./G4SimulationAnalysis 3mm_4phi_screen
-# ./G4SimulationAnalysis 3mm_7phi_screen
-# ./G4SimulationAnalysis 3mm_nocollimator_screen
-
-# ./G4SimulationAnalysis 7mm_2phi_noscreen
-# ./G4SimulationAnalysis 7mm_3phi_noscreen
-# ./G4SimulationAnalysis 7mm_4phi_noscreen
-# ./G4SimulationAnalysis 7mm_7phi_noscreen
-# ./G4SimulationAnalysis 7mm_nocollimator_noscreen
-# ./G4SimulationAnalysis 7mm_2phi_screen
-# ./G4SimulationAnalysis 7mm_3phi_screen
-# ./G4SimulationAnalysis 7mm_4phi_screen
-# ./G4SimulationAnalysis 7mm_7phi_screen
-# ./G4SimulationAnalysis 7mm_nocollimator_screen
-
-# ./G4SimulationAnalysis 20mm_2phi_noscreen
-# ./G4SimulationAnalysis 20mm_3phi_noscreen
-# ./G4SimulationAnalysis 20mm_4phi_noscreen
-# ./G4SimulationAnalysis 20mm_7phi_noscreen
-# ./G4SimulationAnalysis 20mm_nocollimator_noscreen
-# ./G4SimulationAnalysis 20mm_2phi_screen
-# ./G4SimulationAnalysis 20mm_3phi_screen
-# ./G4SimulationAnalysis 20mm_4phi_screen
-# ./G4SimulationAnalysis 20mm_7phi_screen
-# ./G4SimulationAnalysis 20mm_nocollimator_screen
-
-./G4SimulationAnalysis L1F2AX_W082
-./G4SimulationAnalysis L1F2AX_W085
-./G4SimulationAnalysis L1F2AX_W088
-./G4SimulationAnalysis L1F2AX_W091
-./G4SimulationAnalysis L1F2AX_W094
-./G4SimulationAnalysis L1F2AX_W097
-./G4SimulationAnalysis L1F2AX_W100
-./G4SimulationAnalysis L1F2AX_W103
-./G4SimulationAnalysis L1F2AX_W106
-./G4SimulationAnalysis L1F2AX_W109
-./G4SimulationAnalysis L1F2AX_W112
+# ./G4SimulationAnalysis L1F2AX_W082
+# ./G4SimulationAnalysis L1F2AX_W085
+# ./G4SimulationAnalysis L1F2AX_W088
+# ./G4SimulationAnalysis L1F2AX_W091
+# ./G4SimulationAnalysis L1F2AX_W094
+# ./G4SimulationAnalysis L1F2AX_W097
+# ./G4SimulationAnalysis L1F2AX_W100
+# ./G4SimulationAnalysis L1F2AX_W103
+# ./G4SimulationAnalysis L1F2AX_W106
+# ./G4SimulationAnalysis L1F2AX_W109
+# ./G4SimulationAnalysis L1F2AX_W112
 
