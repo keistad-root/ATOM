b753e1a4ea9f57485cd5d5fef5038132b35e6b64
keistad-root <asdff46@pusan.ac.kr>
2025-02-11 18:00:01 +0900

    auto commit at 2025-02-11 18:00:01

--- module/comparison/inc/TExperimentInfo.h
+++ module/comparison/inc/TExperimentInfo.h
@@ -1,10 +1,21 @@
 #ifndef __TEXPERIMENTINFO__
 #define __TEXPERIMENTINFO__
 
+#include<string>
+#include<array>
+
 class TExperimentInfo {
 public:
 	TExperimentInfo();
 	~TExperimentInfo();
-}
+private:
+	std::string mTag;
+	int mLength;
+	double mCollimatorLength;
+	double mCollimatorArea;
+	int mMinute;
+	std::array<double, 60> mEntry;
+	std::array<double, 60> mError;
+};
 
 #endif
--- scripts/g4simulation.sh
+++ scripts/g4simulation.sh
@@ -10,15 +10,15 @@ done
 
 # Send commands to each window
 tmux send-keys -t g4:0 'htop' C-m
-tmux send-keys -t g4:1 './G4Simulation L1F4AX_AX000; ./G4Simulation L1F4AX_AX005; ./G4Simulation L1F4AX_AY000; ./G4Simulation L1F4AX_AY005' C-m
-tmux send-keys -t g4:2 './G4Simulation L1F4AX_AX010; ./G4Simulation L1F4AX_AX015; ./G4Simulation L1F4AX_AY000; ./G4Simulation L1F4AX_AY005' C-m
-tmux send-keys -t g4:3 './G4Simulation L1F4AX_AX020; ./G4Simulation L1F4AX_AX025; ./G4Simulation L1F4AX_AY000; ./G4Simulation L1F4AX_AY005' C-m
-tmux send-keys -t g4:4 './G4Simulation L1F4AX_AX030; ./G4Simulation L1F4AX_AX035; ./G4Simulation L1F4AX_AY000; ./G4Simulation L1F4AX_AY005' C-m
-tmux send-keys -t g4:5 './G4Simulation L1F4AX_AX040; ./G4Simulation L1F4AX_AX045; ./G4Simulation L1F4AX_AY000; ./G4Simulation L1F4AX_AY005' C-m
-tmux send-keys -t g4:6 './G4Simulation L1F4AX_AX050; ./G4Simulation L1F4AX_AX055; ./G4Simulation L1F4AX_AY000; ./G4Simulation L1F4AX_AY005' C-m
-tmux send-keys -t g4:7 './G4Simulation L1F4AX_AX060; ./G4Simulation L1F4AX_AX065; ./G4Simulation L1F4AX_AY000; ./G4Simulation L1F4AX_AY005' C-m
-tmux send-keys -t g4:8 './G4Simulation L1F4AX_AX070; ./G4Simulation L1F4AX_AX075; ./G4Simulation L1F4AX_AY000; ./G4Simulation L1F4AX_AY005' C-m
-tmux send-keys -t g4:9 './G4Simulation L1F4AX_AX080; ./G4Simulation L1F4AX_AX085; ./G4Simulation L1F4AX_AY000; ./G4Simulation L1F4AX_AY005' C-m
+tmux send-keys -t g4:1 './G4Simulation L1F4AX_AY000; ./G4Simulation L1F4AX_AY005' C-m
+tmux send-keys -t g4:2 './G4Simulation L1F4AX_AY010; ./G4Simulation L1F4AX_AY015' C-m
+tmux send-keys -t g4:3 './G4Simulation L1F4AX_AY020; ./G4Simulation L1F4AX_AY025' C-m
+tmux send-keys -t g4:4 './G4Simulation L1F4AX_AY030; ./G4Simulation L1F4AX_AY035' C-m
+tmux send-keys -t g4:5 './G4Simulation L1F4AX_AY040; ./G4Simulation L1F4AX_AY045' C-m
+tmux send-keys -t g4:6 './G4Simulation L1F4AX_AY050; ./G4Simulation L1F4AX_AY055' C-m
+tmux send-keys -t g4:7 './G4Simulation L1F4AX_AY060; ./G4Simulation L1F4AX_AY065' C-m
+tmux send-keys -t g4:8 './G4Simulation L1F4AX_AY070; ./G4Simulation L1F4AX_AY075' C-m
+tmux send-keys -t g4:9 './G4Simulation L1F4AX_AY080; ./G4Simulation L1F4AX_AY085' C-m
 
 # Attach to the tmux session
 tmux attach-session -t g4
