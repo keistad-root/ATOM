25e32572d3e0395042252475b5ceb43e31adca05
keistad-root <asdff46@pusan.ac.kr>
2025-02-12 17:00:01 +0900

    auto commit at 2025-02-12 17:00:01

--- scripts/g4simulation.sh
+++ scripts/g4simulation.sh
@@ -4,21 +4,23 @@
 tmux new-session -d -s g4
 
 # Create 10 additional windows (total 11 windows)
-for i in {1..9}; do
+for i in {1..11}; do
     tmux new-window -t g4
 done
 
 # Send commands to each window
 tmux send-keys -t g4:0 'htop' C-m
-tmux send-keys -t g4:1 './G4Simulation L1F4AX_AY000; ./G4Simulation L1F4AX_AY005' C-m
-tmux send-keys -t g4:2 './G4Simulation L1F4AX_AY010; ./G4Simulation L1F4AX_AY015' C-m
-tmux send-keys -t g4:3 './G4Simulation L1F4AX_AY020; ./G4Simulation L1F4AX_AY025' C-m
-tmux send-keys -t g4:4 './G4Simulation L1F4AX_AY030; ./G4Simulation L1F4AX_AY035' C-m
-tmux send-keys -t g4:5 './G4Simulation L1F4AX_AY040; ./G4Simulation L1F4AX_AY045' C-m
-tmux send-keys -t g4:6 './G4Simulation L1F4AX_AY050; ./G4Simulation L1F4AX_AY055' C-m
-tmux send-keys -t g4:7 './G4Simulation L1F4AX_AY060; ./G4Simulation L1F4AX_AY065' C-m
-tmux send-keys -t g4:8 './G4Simulation L1F4AX_AY070; ./G4Simulation L1F4AX_AY075' C-m
-tmux send-keys -t g4:9 './G4Simulation L1F4AX_AY080; ./G4Simulation L1F4AX_AY085' C-m
+tmux send-keys -t g4:1 './G4Simulation L1F4AX_LS045; ./G4Simulation L1F4AX_LA195' C-m
+tmux send-keys -t g4:2 './G4Simulation L1F4AX_LS046; ./G4Simulation L1F4AX_LA196' C-m
+tmux send-keys -t g4:3 './G4Simulation L1F4AX_LS047; ./G4Simulation L1F4AX_LA197' C-m
+tmux send-keys -t g4:4 './G4Simulation L1F4AX_LS048; ./G4Simulation L1F4AX_LA198' C-m
+tmux send-keys -t g4:5 './G4Simulation L1F4AX_LS049; ./G4Simulation L1F4AX_LA199' C-m
+tmux send-keys -t g4:6 './G4Simulation L1F4AX_LS050; ./G4Simulation L1F4AX_LA200' C-m
+tmux send-keys -t g4:7 './G4Simulation L1F4AX_LS051; ./G4Simulation L1F4AX_LA201' C-m
+tmux send-keys -t g4:8 './G4Simulation L1F4AX_LS052; ./G4Simulation L1F4AX_LA202' C-m
+tmux send-keys -t g4:9 './G4Simulation L1F4AX_LS053; ./G4Simulation L1F4AX_LA203' C-m
+tmux send-keys -t g4:10 './G4Simulation L1F4AX_LS054; ./G4Simulation L1F4AX_LA204' C-m
+tmux send-keys -t g4:11 './G4Simulation L1F4AX_LS055; ./G4Simulation L1F4AX_LA205' C-m
 
 # Attach to the tmux session
 tmux attach-session -t g4
