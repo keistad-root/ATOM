3e15e61e5dfcd542cf39542d4a9e8e1a7c49d34c
keistad-root <asdff46@pusan.ac.kr>
2025-02-14 12:00:02 +0900

    auto commit at 2025-02-14 12:00:02

--- exe/G4Systematic.cpp
+++ exe/G4Systematic.cpp
@@ -9,52 +9,64 @@
 #include "TText.h"
 #include<string>
 
-void setGraph(const std::string& tag) {
-
-}
-
-int main() {
-	CppConfigFile config("/home/ychoi/ATOM/config/Systematic.conf");
-	TGeantInfoSet set;
-
-	std::vector<TGeantInfo> interestSet;
+void setGraph(const CppConfigFile& config, const TGeantInfoSet& set, const std::string& name) {
+	std::vector<TGeantInfo> interestData;
 	TGraphErrors* graph[3] = {new TGraphErrors(), new TGraphErrors(), new TGraphErrors()};
 
 	for ( auto& entry : set.getGeantSet() ) {
 		std::string tag = entry.getTag();
-		if ( tag.find("DX") != std::string::npos ) {
-			interestSet.push_back(entry);
+		if ( tag.find(name) != std::string::npos ) {
+			interestData.push_back(entry);
 		}
 	}
-
-	for ( auto& entry : interestSet ) {
-		graph[0]->SetPoint(graph[0]->GetN(), entry.getAlpidePositionX(), entry.getEEM()[0]);
+	for ( auto& entry : interestData ) {
+		double xAxis;
+		if ( name == "DX" ) xAxis = entry.getAlpidePositionX();
+		else if ( name == "DY" ) xAxis = entry.getAlpidePositionY();
+		else if ( name == "AX" ) xAxis = entry.getAlpideAngleX();
+		else if ( name == "AY" ) xAxis = entry.getAlpideAngleY();
+		graph[0]->SetPoint(graph[0]->GetN(), xAxis, entry.getEEM()[0]);
 		graph[0]->SetPointError(graph[0]->GetN() - 1, 0, entry.getEEM()[1]);
-		graph[1]->SetPoint(graph[1]->GetN(), entry.getAlpidePositionX(), entry.getEAE()[0]);
+		graph[1]->SetPoint(graph[1]->GetN(), xAxis, entry.getEAE()[0]);
 		graph[1]->SetPointError(graph[1]->GetN() - 1, 0, entry.getEAE()[1]);
-		graph[2]->SetPoint(graph[2]->GetN(), entry.getAlpidePositionX(), entry.getDouble()[0]);
+		graph[2]->SetPoint(graph[2]->GetN(), xAxis, entry.getDouble()[0]);
 		graph[2]->SetPointError(graph[2]->GetN() - 1, 0, entry.getDouble()[1]);
 	}
 
-	TCanvas* canvas[3] = {new TCanvas("eemCanvas", "", 1000, 1000), new TCanvas("eaeCanvas", "", 1000, 1000), new TCanvas("doubleCanvas", "", 1000, 1000)};
 	TF1* fitFunc[3] = {new TF1("fitFunc1", "[0]*x+[1]", 0, 3), new TF1("fitFunc2", "[0]*x+[1]", 0, 3), new TF1("fitFunc3", "[0]*x+[1]", 0, 3)};
 	graph[0]->Fit(fitFunc[0], "R");
 	graph[1]->Fit(fitFunc[1], "R");
 	graph[2]->Fit(fitFunc[2], "R");
-	TText* text = new TText(.5, .5, static_cast<TString>("Slope: " + std::to_string(fitFunc[0]->GetParameter(0))));
-	text->SetTextSize(2);
+
+	TCanvas* canvas[3] = {new TCanvas("eemCanvas", "", 1000, 1000), new TCanvas("eaeCanvas", "", 1000, 1000), new TCanvas("doubleCanvas", "", 1000, 1000)};
 
 	TPlotter plotter;
 	std::filesystem::path outputPath = "/home/ychoi/ATOM/build/Data/";
-	plotter.savePlot(canvas[0], graph[0], config.getConfig("ALPIDE_POSITION_X_EEM"));
-	canvas[0]->cd();
-	text->Draw("SAME");
-	plotter.saveCanvas(canvas[0], outputPath, config.getConfig("ALPIDE_POSITION_X_EEM"));
-	plotter.savePlot(canvas[1], graph[1], config.getConfig("ALPIDE_POSITION_X_EAE"));
-	plotter.saveCanvas(canvas[1], outputPath, config.getConfig("ALPIDE_POSITION_X_EAE"));
-	plotter.savePlot(canvas[2], graph[2], config.getConfig("ALPIDE_POSITION_X_DOUBLE"));
-	plotter.saveCanvas(canvas[2], outputPath, config.getConfig("ALPIDE_POSITION_X_DOUBLE"));
+	plotter.savePlot(canvas[0], graph[0], config.getConfig(name + "_EEM"));
+	plotter.saveCanvas(canvas[0], outputPath, config.getConfig(name + "_EEM"));
+	plotter.savePlot(canvas[1], graph[1], config.getConfig(name + "_EAE"));
+	plotter.saveCanvas(canvas[1], outputPath, config.getConfig(name + "_EAE"));
+	plotter.savePlot(canvas[2], graph[2], config.getConfig(name + "_DOUBLE"));
+	plotter.saveCanvas(canvas[2], outputPath, config.getConfig(name + "_DOUBLE"));
+	delete graph[0];
+	delete graph[1];
+	delete graph[2];
+	delete fitFunc[0];
+	delete fitFunc[1];
+	delete fitFunc[2];
+	delete canvas[0];
+	delete canvas[1];
+	delete canvas[2];
+}
 
+int main() {
+	CppConfigFile config("/home/ychoi/ATOM/build/config/Systematic.conf");
+	TGeantInfoSet set;
+
+	std::vector<TGeantInfo> interestSet;
+	setGraph(config, set, "DX");
+	setGraph(config, set, "DY");
+	setGraph(config, set, "AX");
 
 	return 0;
 }
--- scripts/g4analysis.sh
+++ scripts/g4analysis.sh
@@ -2,6 +2,30 @@
 
 cd ~/ATOM/build
 
+./G4SimulationAnalysis L1F4AX_LS045
+./G4SimulationAnalysis L1F4AX_LS046
+./G4SimulationAnalysis L1F4AX_LS047
+./G4SimulationAnalysis L1F4AX_LS048
+./G4SimulationAnalysis L1F4AX_LS049
+./G4SimulationAnalysis L1F4AX_LS050
+./G4SimulationAnalysis L1F4AX_LS051
+./G4SimulationAnalysis L1F4AX_LS052
+./G4SimulationAnalysis L1F4AX_LS053
+./G4SimulationAnalysis L1F4AX_LS054
+./G4SimulationAnalysis L1F4AX_LS055
+./G4SimulationAnalysis L1F4AX_LA195
+./G4SimulationAnalysis L1F4AX_LA196
+./G4SimulationAnalysis L1F4AX_LA197
+./G4SimulationAnalysis L1F4AX_LA198
+./G4SimulationAnalysis L1F4AX_LA199
+./G4SimulationAnalysis L1F4AX_LA200
+./G4SimulationAnalysis L1F4AX_LA201
+./G4SimulationAnalysis L1F4AX_LA202
+./G4SimulationAnalysis L1F4AX_LA203
+./G4SimulationAnalysis L1F4AX_LA204
+./G4SimulationAnalysis L1F4AX_LA205
+
+
 # ./G4SimulationAnalysis L1F2AX
 # ./G4SimulationAnalysis L1F2AO
 # ./G4SimulationAnalysis L1F3AX
@@ -13,38 +37,38 @@ cd ~/ATOM/build
 # ./G4SimulationAnalysis L1REFAX
 # ./G4SimulationAnalysis L1REFAO
 
-./G4SimulationAnalysis L3F2AX
-./G4SimulationAnalysis L3F2AO
-./G4SimulationAnalysis L3F3AX
-./G4SimulationAnalysis L3F3AO
-./G4SimulationAnalysis L3F4AX
-./G4SimulationAnalysis L3F4AO
-./G4SimulationAnalysis L3F7AX
-./G4SimulationAnalysis L3F7AO
-./G4SimulationAnalysis L3REFAX
-./G4SimulationAnalysis L3REFAO
+# ./G4SimulationAnalysis L3F2AX
+# ./G4SimulationAnalysis L3F2AO
+# ./G4SimulationAnalysis L3F3AX
+# ./G4SimulationAnalysis L3F3AO
+# ./G4SimulationAnalysis L3F4AX
+# ./G4SimulationAnalysis L3F4AO
+# ./G4SimulationAnalysis L3F7AX
+# ./G4SimulationAnalysis L3F7AO
+# ./G4SimulationAnalysis L3REFAX
+# ./G4SimulationAnalysis L3REFAO
 
-./G4SimulationAnalysis L7F2AX
-./G4SimulationAnalysis L7F2AO
-./G4SimulationAnalysis L7F3AX
-./G4SimulationAnalysis L7F3AO
-./G4SimulationAnalysis L7F4AX
-./G4SimulationAnalysis L7F4AO
-./G4SimulationAnalysis L7F7AX
-./G4SimulationAnalysis L7F7AO
-./G4SimulationAnalysis L7REFAX
-./G4SimulationAnalysis L7REFAO
+# ./G4SimulationAnalysis L7F2AX
+# ./G4SimulationAnalysis L7F2AO
+# ./G4SimulationAnalysis L7F3AX
+# ./G4SimulationAnalysis L7F3AO
+# ./G4SimulationAnalysis L7F4AX
+# ./G4SimulationAnalysis L7F4AO
+# ./G4SimulationAnalysis L7F7AX
+# ./G4SimulationAnalysis L7F7AO
+# ./G4SimulationAnalysis L7REFAX
+# ./G4SimulationAnalysis L7REFAO
 
-./G4SimulationAnalysis L20F2AX
-./G4SimulationAnalysis L20F2AO
-./G4SimulationAnalysis L20F3AX
-./G4SimulationAnalysis L20F3AO
-./G4SimulationAnalysis L20F4AX
-./G4SimulationAnalysis L20F4AO
-./G4SimulationAnalysis L20F7AX
-./G4SimulationAnalysis L20F7AO
-./G4SimulationAnalysis L20REFAX
-./G4SimulationAnalysis L20REFAO
+# ./G4SimulationAnalysis L20F2AX
+# ./G4SimulationAnalysis L20F2AO
+# ./G4SimulationAnalysis L20F3AX
+# ./G4SimulationAnalysis L20F3AO
+# ./G4SimulationAnalysis L20F4AX
+# ./G4SimulationAnalysis L20F4AO
+# ./G4SimulationAnalysis L20F7AX
+# ./G4SimulationAnalysis L20F7AO
+# ./G4SimulationAnalysis L20REFAX
+# ./G4SimulationAnalysis L20REFAO
 
 # ./G4SimulationAnalysis L1F2AX_W082
 # ./G4SimulationAnalysis L1F2AX_W085
