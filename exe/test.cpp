34329843dfcb496cac05b10b50a681060ac275e7
keistad-root <asdff46@pusan.ac.kr>
2025-03-24 18:00:01 +0900

    auto commit at 2025-03-24 18:00:01

--- exe/test.cpp
+++ exe/test.cpp
@@ -4,7 +4,7 @@
 
 int main() {
 	std::array<int, 10> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	std::cout << std::accumulate(arr.begin(), arr.begin() + 9, 0) << std::endl;
+	std::cout << std::accumulate(arr.begin() + 1, arr.end(), 0) << std::endl;
 
 	return 0;
 }
